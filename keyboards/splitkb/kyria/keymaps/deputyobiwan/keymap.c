0cdade4cda621174648a5821aa2acd67e8f20053
deputyobiwan <bop1@gmx.de>
2023-01-29 11:53:59 +0100

    Make layer keys transparent on each layer

--- keyboards/splitkb/kyria/keymaps/deputyobiwan/keymap.c
+++ keyboards/splitkb/kyria/keymaps/deputyobiwan/keymap.c
@@ -75,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_NO, KC_NO, KC_NO, KC_UP, KC_NO, KC_NO,                                                                                KC_ESCAPE, KC_NO, KC_PAGE_UP, KC_NO, KC_NO, KC_NO,
        KC_NO, KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT, KC_END,                                                                 KC_DELETE, KC_NO, KC_PAGE_DOWN, KC_NO, KC_NO, KC_NO,
        KC_NO, KC_MEDIA_PLAY_PAUSE, KC_AUDIO_MUTE, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_NO, KC_NO, KC_NO,      KC_NO, KC_NO, KC_BACKSPACE, LCTL(KC_C), LCTL(KC_V), LCTL(KC_X), KC_NO, KC_NO,
-                            KC_NO, KC_NO, KC_NO, KC_LCTL, KC_LSFT,                                              KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+                            KC_NO, KC_NO, KC_NO, KC_LCTL, KC_LSFT,                                              KC_NO, KC_TRANSPARENT, KC_NO, KC_NO, KC_NO
     ),
 
     /*
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_NO, KC_NO, KC_NO, KC_MS_UP, KC_NO, KC_NO,                                                         KC_NO, KC_MS_WH_LEFT, KC_MS_WH_UP, KC_MS_WH_RIGHT, KC_NO, KC_NO,
        KC_NO, KC_NO, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_NO,                                  KC_WWW_BACK, KC_MS_BTN1, KC_MS_WH_DOWN, KC_MS_BTN2, KC_WWW_FORWARD, KC_NO,
        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                        KC_NO, KC_NO, KC_NO, LCTL(KC_C), LCTL(KC_V), LCTL(KC_X), KC_NO, KC_NO,
-                            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+                            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                        KC_TRANSPARENT, KC_NO, KC_NO, KC_NO, KC_NO
     ),
 
     /*
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_ESCAPE,                                                      DE_AMPR, DE_LBRC, DE_ACUT, DE_SLSH, DE_HASH, KC_NO,
        KC_NO, DE_SECT, DE_CIRC, DE_PERC, DE_AT, KC_DELETE,                                                   DE_DLR, DE_LPRN, DE_DQUO, DE_EQL, DE_ASTR, KC_NO,
        KC_NO, KC_NO, LCTL(KC_X), LCTL(KC_V), LCTL(KC_C), KC_BACKSPACE, KC_NO, KC_NO,           KC_NO, KC_NO, DE_PLUS, DE_LCBR, DE_QUOT, DE_LABK, DE_RABK, KC_NO,
-                            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                              DE_EXLM, DE_QUES, KC_NO, KC_NO, KC_NO
+                            KC_NO, KC_NO, KC_NO, KC_TRANSPARENT, KC_NO,                              DE_EXLM, DE_QUES, KC_NO, KC_NO, KC_NO
     ),
 
     /*
@@ -135,7 +135,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                       KC_NO, KC_7, KC_8, KC_9, KC_NO, KC_NO,
        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                                       KC_NO, KC_4, KC_5, KC_6, KC_NO, KC_NO,
        KC_NO, KC_NO, LCTL(KC_X), LCTL(KC_V), LCTL(KC_C), KC_NO, KC_NO, KC_NO,            KC_NO, KC_NO, KC_NO, KC_1, KC_2, KC_3, KC_NO, KC_NO,
-                            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                           KC_NO, KC_0, KC_NO, KC_NO, KC_NO
+                            KC_NO, KC_NO, KC_NO, KC_NO, KC_TRANSPARENT,                           KC_NO, KC_0, KC_NO, KC_NO, KC_NO
     ),
 
     /*
@@ -155,7 +155,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                               KC_MENU, KC_F7, KC_F8, KC_F9, KC_F10, KC_NO,
        KC_NO, KC_NO, KC_NO, KC_NO, MOUSEJIGGLERMACRO, KC_NO,                                     KC_NO, KC_F4, KC_F5, KC_F6, KC_F11, KC_NO,
        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                     KC_NO, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F12, KC_NO,
-                            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+                            KC_NO, KC_NO, KC_NO, KC_TRANSPARENT, KC_TRANSPARENT,                     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
     )
 };
 
